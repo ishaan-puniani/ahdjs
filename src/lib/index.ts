0eddfcd544f196ba57c7cb53cdddbad346065d85
Ishaan Puniani <ishaan.puniani@kindredgroup.com>
2023-08-01 16:04:37 +0200

    feat: added initial integration with api

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,9 +5,12 @@ import Information from "./plugins/information";
 import Beacons from "./plugins/beacons";
 class AHDjs {
   private guideChimp: any;
-  constructor(...args: any) {
+  private appId: string;
+  private contextToursMapData: any;
+  constructor(applicationId: any, tour: any, options: any) {
+    this.appId = applicationId;
     //@ts-ignore
-    this.guideChimp = new GuideChimp(...args);
+    this.guideChimp = new GuideChimp(tour, options);
 
     this.guideChimp.prototype = GuideChimp.prototype;
     this.guideChimp.plugins = new Set();
@@ -29,21 +32,76 @@ class AHDjs {
       if (!this.guideChimp.plugins.has(plugin)) {
         this.guideChimp.plugins.add(plugin);
         // @ts-ignore
-        plugin(GuideChimp, this.guideChimp, ...args);
+        plugin(GuideChimp, this.guideChimp, tour, options);
       }
     });
   }
 
+  updatePageUrl = async (url: string, refetch: boolean) => {
+    await this.stop();
+    // todo: local caching and use https://github.com/pillarjs/path-to-regexp to find exact match
+    const respons: any = await fetch(
+      `https://ahd-be-jggub5n6qq-em.a.run.app/api/tenant/${this.appId}/contexttour`
+    ).then((res) => res.json());
+
+    this.contextToursMapData = respons.rows;
+    //if there is anything to open onload
+    const onboardTour = respons.rows
+      .filter((row: any) => !!row.content)
+      .map((row: any) => {
+        return {
+          element: row.selector,
+          title: row.content.title,
+          description: row.content.content,
+          tour: [
+            {
+              title: "Multi Feature",
+              description: "Multi Feature beacon clicked.",
+            },
+          ],
+        };
+      });
+    this.setTour(onboardTour);
+    this.start();
+  };
+
+  setTour = (tour: any) => {
+    this.guideChimp.setTour(tour);
+  };
   beacons = (...args: any) => {
     // @ts-ignore
     // this.guideChimp.beacon = new Beacons(...args);
-    const b = this.guideChimp.beacons(...args);
+
+    const tourBeacons = this.contextToursMapData
+      .filter((row: any) => !!row.content)
+      .map((row: any) => {
+        return {
+          element: row.selector,
+          title: row.content.title,
+          description: row.content.content,
+          tour: [
+            {
+              title: "BC Multi Feature",
+              description: "BC Multi Feature beacon clicked.",
+            },
+            {
+              title: "B2C Multi Feature",
+              description: "B2C Multi Feature beacon clicked.",
+            },
+          ],
+        };
+      });
+
+    const b = this.guideChimp.beacons(tourBeacons);
     setTimeout(() => {
       b.showAll();
     }, 1000);
   };
   start = (...args: any) => {
     this.guideChimp.start(...args);
+  };
+  stop = async (...args: any) => {
+    await this.guideChimp.stop(...args);
   };
   showInformation = (...args: any) => {
     // @ts-ignore
